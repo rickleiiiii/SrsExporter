cc2f60c5f1d4ebb091dcc8437c051cb8ceeeca7b
307005821 <307005821@BHXBCLBWYU1CNPL>
2023-05-23 22:00:07 +0800

    Add project files.

---
+++ SrsExporter/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SrsExporter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
