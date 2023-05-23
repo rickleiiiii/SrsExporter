3439530347d0ea98537133713284fc616325516f
Rick Lei <rick.lei@bakerhughes.com>
2023-05-23 22:40:43 +0800

    Implement function to load settings from a json configuration file. The settings includes username, password, TFS sever uri, project collection and project.

--- SrsExporter/Program.cs
+++ SrsExporter/Program.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
+
+using Microsoft.Extensions.Configuration;
 
 namespace SrsExporter
 {
@@ -10,6 +13,18 @@ namespace SrsExporter
     {
         static void Main(string[] args)
         {
+            var config = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            var username = config["Username"];
+            var password = config["Password"];
+            var uri = config["Uri"];
+            var collection = config["Collection"];
+            var project = config["Project"];
+
+            Console.ReadKey();
         }
     }
 }
