3c37fab5d5724a8956119c68f8bb7da154906349
Rick Lei <rick.lei@bakerhughes.com>
2023-06-03 22:43:03 +0800

    Add a new class, SrsExporter, to call TFS query and assemble the SRS document.

---
+++ SrsExporter/SrsExporter.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TfsExporter;
+
+namespace SrsExporter
+{
+    internal class SrsExporter
+    {
+        private readonly QueryExecutor qe;
+
+        public SrsExporter() {
+            this.qe = new QueryExecutor("appsettings.json");
+        }
+
+        public async Task ListEpics()
+        {
+            var workItems = await this.qe.QueryEpics().ConfigureAwait(false);
+
+            Console.WriteLine("Query Results: {0} items found", workItems.Count);
+
+            // loop though work items and write to console
+            foreach (var workItem in workItems)
+            {
+                Console.WriteLine(
+                    "{0}\t{1}\t{2}\t{3}",
+                    workItem.Id,
+                    workItem.Fields["System.Title"],
+                    workItem.Fields["System.Description"],
+                    workItem.Fields["Microsoft.VSTS.Common.StackRank"]);
+            }
+        }
+    }
+}
